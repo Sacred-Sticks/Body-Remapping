5051ea6953114d6ca3b63237c57b5c3550c4d979
Lucas Ackman <AckmanLD@gmail.com>
2023-02-25 15:03:17 -0600

    Fixed Player Assignment

--- Body-Remapping/Assets/Scripts/IK/PlayerAssignment.cs
+++ Body-Remapping/Assets/Scripts/IK/PlayerAssignment.cs
@@ -1,21 +1,38 @@
+using System;
 using UnityEngine;
 using InputManagement;
 using ReferenceVariables;
+using UnityEngine.Serialization;
 
 public class PlayerAssignment : MonoBehaviour
 {
     [SerializeField] private Transform headset, leftController, rightController, character;
-    [SerializeField] private Vector3Reference position;
-    //[SerializeField] private QuaternionReference rotation;
-    
-    private void SetPosition()
+    [SerializeField] private Vector3Reference hmdPosition;
+    [SerializeField] private QuaternionReference hmdRotation;
+    [SerializeField] private Vector3Reference leftControllerPosition;
+    [SerializeField] private QuaternionReference leftControllerRotation;
+    [SerializeField] private Vector3Reference rightControllerPosition;
+    [SerializeField] private QuaternionReference rightControllerRotation;
+
+    private void Update()
     {
-        transform.position = position.Value.x * character.right + position.Value.y * character.up + position.Value.z * character.forward;
+        SetPosition(headset, hmdPosition.Value);
+        SetPosition(leftController, leftControllerPosition.Value);
+        SetPosition(rightController, rightControllerPosition.Value);
+        
+        SetRotation(headset, hmdRotation.Value);
+        SetRotation(leftController, leftControllerRotation.Value);
+        SetRotation(rightController, rightControllerRotation.Value);
     }
 
-    private void SetRotation()
+    private void SetPosition(Transform objectTransform, Vector3 objectPosition)
     {
-        //transform.rotation = rotation * character.rotation;
+        objectTransform.position = objectPosition.x * character.right + objectPosition.y * character.up + objectPosition.z * character.forward;
+    }
+
+    private void SetRotation(Transform objectTransform, Quaternion objectRotation)
+    {
+        objectTransform.rotation = objectRotation * character.rotation;
     }
 
 }
