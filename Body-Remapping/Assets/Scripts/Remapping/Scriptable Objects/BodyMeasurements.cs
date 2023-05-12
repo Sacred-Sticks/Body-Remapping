6be6a1e05fcce7415940c8bc1ab5a55b00169e60
Lucas Ackman <AckmanLD@gmail.com>
2023-05-12 01:45:02 -0500

    Removed Unused, Broken, Version 1.0 scripts

--- Body-Remapping/Assets/Scripts/Remapping/Scriptable Objects/BodyMeasurements.cs
+++
@@ -1,8 +0,0 @@
-using ReferenceVariables;
-using UnityEngine;
-[CreateAssetMenu(fileName = "Body Proportions", menuName = "Remapping/Body Proportions")]
-public class BodyMeasurements : ScriptableObject
-{
-    public FloatReference leftArmLength;
-    public FloatReference rightArmLength;
-}
